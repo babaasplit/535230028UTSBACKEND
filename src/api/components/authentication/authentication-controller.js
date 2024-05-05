5fe27fc4d85134b3c591b50ebdc616234ddb78a3
babaasplit <162522120+babaasplit@users.noreply.github.com>
2024-05-05 19:42:26 +0700

    login cuy

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,6 +1,8 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
+let attempts = {};
+
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -8,24 +10,51 @@ const authenticationServices = require('./authentication-service');
  * @param {object} next - Express route middlewares
  * @returns {object} Response object or pass an error to the next route
  */
-async function login(request, response, next) {
-  const { email, password } = request.body;
+
+async function login(req, res, next) {
+  const { email, password } = req.body;
 
   try {
-    // Check login credentials
+    if (!attempts[email]) {
+      attempts[email] = { failed: 0, AkhirAttempt: new Date() };
+    } else {
+      const AkhirAttempt = attempts[email];
+      const LOLelapsed = (new Date() - AkhirAttempt.AkhirAttempt) / (1000 * 60);
+      if (LOLelapsed >= 30) {
+        attempts[email] = {
+          failed: 0,
+          AkhirAttempt: new Date(),
+        };
+      }
+    }
+
+    const limitLewatBatas = attempts[email].failed >= 5;
+
+    if (limitLewatBatas) {
+      throw errorResponder(
+        errorTypes.FORBIDDEN,
+        'Too many failed login attempts. Please try again in 30 minutes'
+      );
+    }
+
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
       password
     );
 
     if (!loginSuccess) {
+      attempts[email].failed++;
+      const remainingAttempts = 5 - attempts[email].failed;
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
-        'Wrong email or password'
+        `Wrong email or password, ${remainingAttempts} chances left`
       );
     }
 
-    return response.status(200).json(loginSuccess);
+    attempts[email].failed = 0;
+    attempts[email].AkhirAttempt = new Date();
+
+    return res.status(200).json(loginSuccess);
   } catch (error) {
     return next(error);
   }
